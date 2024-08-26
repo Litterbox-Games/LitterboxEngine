77df984e7a31c95afdb80710a239c501db6530d0
FireSpittinKitten <60477169+LarkenB@users.noreply.github.com>
2024-08-25 22:06:45 -0400

    Add: camera zooming
    No user input to actually change the zoom level at the moment

--- Client/Graphics/Camera.cs
+++ Client/Graphics/Camera.cs
@@ -32,8 +32,8 @@ public class Camera
             // Center the camera (Vulkan's origin is in the center of the screen)
             * Matrix4x4.CreateTranslation(-0.5f, -0.5f, 0f)
             // Convert from screen coords to world coords
-            * Matrix4x4.CreateScale(new Vector3(2.0f / Size.X * 96, 2.0f / Size.Y * 96, 0), Vector3.Zero);
+            * Matrix4x4.CreateScale(new Vector3(2.0f / Size.X * 96, 2.0f / Size.Y * 96, 0), Vector3.Zero)
             // Zooms out camera
-            // * Matrix4x4.CreateScale(new Vector3(0.0625f, 0.0625f, 0), Vector3.Zero);
+            * Matrix4x4.CreateScale(new Vector3(Zoom, Zoom, 0), Vector3.Zero);
     }
 }
