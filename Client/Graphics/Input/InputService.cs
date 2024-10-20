d50180d073107d0362463d3deed0c3c66f8e8170
FireSpittinKitten <60477169+LarkenB@users.noreply.github.com>
2024-10-20 13:42:04 -0400

    Add: support for simple mouse click events

--- Client/Graphics/Input/InputService.cs
+++ Client/Graphics/Input/InputService.cs
@@ -1,4 +1,5 @@
-﻿using Common.DI;
+﻿using System.Numerics;
+using Common.DI;
 using Silk.NET.Input;
 
 namespace Client.Graphics.Input;
@@ -6,15 +7,30 @@ namespace Client.Graphics.Input;
 public class InputService: IService
 {
     private readonly IInputContext _input;
-    
+
+    public Vector2 MousePosition => _input.Mice[0].Position;
+    public event Action<MouseButton, Vector2>? EventOnMouseClick; 
+
     public InputService(WindowService windowService)
     {
         _input = windowService.Input;
+        _input.Mice[0].Click += OnClick;
+    }
+
+    private void OnClick(IMouse _, MouseButton button, Vector2 position)
+    {
+        EventOnMouseClick?.Invoke(button, position);
     }
 
     public bool IsKeyDown(Key key)
     {
         // TODO: give the user the option to choose a keyboard
         return _input.Keyboards[0].IsKeyPressed(key);
+    }
+
+    public bool IsMouseDown(MouseButton button)
+    {
+        // TODO: give the user the option to choose a mouse
+        return _input.Mice[0].IsButtonPressed(button);
     }
 }
