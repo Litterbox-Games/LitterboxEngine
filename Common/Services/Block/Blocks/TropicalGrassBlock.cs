970af6bfa00110dee8c1375b5d8f83d97513d436
SomeCatIDK <graybad1@icloud.com>
2025-04-01 18:34:02 -0400

    Change: world generation uses mappings

--- Common/Services/Block/Blocks/TropicalGrassBlock.cs
+++ Common/Services/Block/Blocks/TropicalGrassBlock.cs
@@ -4,7 +4,7 @@ namespace Common.Services.Block.Blocks;
 
 public class TropicalGrassBlock : IBlock
 {
-    public string Id => "topical_grass";
+    public string Id => "tropical_grass";
     public ushort MappedId { get; set; }
     public string? TexturePath => "Aseprites/BiomePalette.aseprite";
     public Vector2i? TextureOffset => new Vector2i(2, 2);
