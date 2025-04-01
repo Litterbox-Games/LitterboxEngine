e8d59fd34cc56bb43f70cf824eb3d7a3b001863b
SomeCatIDK <graybad1@icloud.com>
2025-04-01 11:46:10 -0400

    Registry interface prototype

---
+++ Common/Registry/IRegistry.cs
@@ -0,0 +1,12 @@
+﻿using Common.Core;
+
+namespace Common.Registry;
+
+public interface IRegistry<T> : IService where T : IRegisterable
+{
+    IRegisterable Resolve(string id);
+    IRegisterable Resolve(uint id);
+    
+    // Passes in an unregistered IRegisterable and returns it with its 'MappedId' set.
+    IRegisterable Register(IRegisterable registerable, bool overrideIfExists = false);
+}
